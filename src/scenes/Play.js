13852950465e96a332ffe69daac83104af1c87cc
Rostager <queenbee.stager@gmail.com>
2026-01-17 18:09:27 -0800

    Rocket Movement Complete

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,6 +4,31 @@ class Play extends Phaser.Scene {
     }
 
     create(){
-        this.add.text(20,20,"Rocket Patrol Play")
+        //place tile sprite
+        this.starfield = this.add.tileSprite(0,0,640, 480, 'starfield').setOrigin(0,0)
+        //Green UI Background
+        this.add.rectangle(0,borderUISize + borderPadding, game.config.width,borderUISize *2, 0x00FF00).setOrigin(0,0)
+        //White Borders
+        this.add.rectangle(0,0,game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0)
+        this.add.rectangle(0,game.config.height - borderUISize, game.config.width,borderUISize, 0xFFFFFF).setOrigin(0,0)
+        this.add.rectangle(0,0,borderUISize, game.config.height,0xFFFFFF).setOrigin(0,0)
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0)
+        //this.add.rectangle(config.width / 2, config.height / 2, 50,50,0xFACADE).setOrigin(0,0) //testing adding a square to the middle of screen
+        
+        //add rocket (p1)
+        this.p1Rocket = new Rocket(this,config.width / 2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0)
+
+        //define keys
+        keyFIRE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F)
+        keyRESET = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
+
+    }
+
+
+    update(){
+        this.starfield.tilePositionX -= 4
+        this.p1Rocket.update()
     }
 }
